f9f92086e6432b19306e9bcdcb34491706a5f064
mkamaUO <mkama022@uottawa.ca>
2025-03-16 02:27:16 -0400

    Optimized performance and quality of outputs to lower the chances of mistakes

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -7,6 +7,7 @@ import java.util.Scanner;
 import io.github.ollama4j.OllamaAPI;
 import io.github.ollama4j.models.response.OllamaResult;
 import io.github.ollama4j.types.OllamaModelType;
+import io.github.ollama4j.utils.Options;
 import io.github.ollama4j.utils.OptionsBuilder;
 
 public class Main {
@@ -16,8 +17,11 @@ public class Main {
 
         OllamaAPI ollamaAPI = new OllamaAPI(host);
 
-        OllamaResult result = new OllamaResult("image", 0, 0);
+        ollamaAPI.setRequestTimeoutSeconds(30);
+        ollamaAPI.setVerbose(false);
+        Options options = new OptionsBuilder().setStop(".jpg").setStop(".png").build();
 
+        OllamaResult result = new OllamaResult("image", 0, 0);
 
         File currentFile = new File("");
         String currentPath = currentFile.getAbsolutePath() + "\\";
@@ -30,16 +34,17 @@ public class Main {
         File originalFile = new File(currentPath + oldName);
         if (originalFile.exists()){ 
             System.out.println(oldName + " found.");
-            String prompt = "What would you rename this image file? Ensure that you only output the new filename. Make sure you DO NOT include the file extension at the end.";
+            String prompt = "What would you name this image? Ensure that you only output the one new name. Try and keep the names short, between 1 and 5 words. Never include .jpg and .png in any outputs. Stick to only characters from the English alphabet, underscores, and hyphens.";
             try {
                 result = ollamaAPI.generateWithImageFiles(OllamaModelType.LLAVA_PHI3, prompt, List.of(new File(currentPath + oldName)), new OptionsBuilder().build());
             } catch (Exception e) {
                 System.out.println("Something happened: " + e);
             }
+                                                        
             String[] fileType = oldName.split("[.]");
             System.out.println("This is the response: " + result.getResponse() + "." + fileType[fileType.length-1]);
 
-            if (originalFile.renameTo(new File(currentPath + result + "." + fileType[fileType.length-1]))) { // missing try catch for when the new name already exists
+            if (originalFile.renameTo(new File(currentPath + result.getResponse().replaceAll("\\s","_") + "." + fileType[fileType.length-1]))) { // missing try catch for when the new name already exists
                 System.out.println("File renamed successfully.");
             } else {
                 System.out.println("Failed to automatically rename file.\n");
